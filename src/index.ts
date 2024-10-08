08e57112ec6b488fe4633951b18607be40e88897
Gustavonobg <gustavonobg@gmail.com>
2024-10-08 10:38:28 -0300

    SetupApi

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express'
+import { Router, Request, Response } from 'express'
+import mongoose from 'mongoose'
+import { config } from './config/config'
+import { router } from './routes/Router'
+
+const server = express()
+const route = Router()
+server.use(express.json())
+server.use(route)
+
+mongoose
+  .connect(config.mongo.url)
+  .then(() => console.log('Connected!'))
+  .catch((err) => console.error('Connection error:', err))
+
+route.get('/', (req: Request, res: Response) => {
+  res.json({ message: 'hello world' })
+})
+
+server.listen(3000, () => console.log(`Server is running on port ${3333}`))
